835f115a9bfee3d75169dc582e7384cea1aae22c
PALLAMRAJU1258 <pallamraju1305@gmail.com>
2023-07-03 14:54:27 +0530

    Python Training

--- Phase-3/Checking_Prime_Num_Using_Recursive.py
+++ Phase-3/Checking_Prime_Num_Using_Recursive.py
@@ -1,1 +1,15 @@
 def Prime_Num(n):
+    if n == 1:
+        return False
+    elif n > 1:
+        for i in range(2,n):
+            if (n%i) == 0:
+                return False
+    return True
+
+
+num = int(input())
+if Prime_Num(num):
+    print(num,"is prime number")
+else:
+    print(num,"is not prime number")
